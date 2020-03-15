eca512bbc8a0392ed760dbac99e396a97103a2fc
Santiago E <sespinosarte99@gmail.com>
2020-03-14 22:55:35 -0500

    Script terminado
    Si quieres lo revisas Mr Pajoy

--- notas2.sh
+++ notas2.sh
@@ -12,5 +12,24 @@ else if [ $1 == "-v" ];then
             echo "En la carpeta $i hay $totaldearchivos archivos "
         fi
     done
+else 
+ if [ -d ~/Documentos/$1 ];then
+    if [ -e ~/Documentos/$1/$2 ];then
+        #Directorio y archivo existen
+        nano ~/Documentos/$1/$2
+    else
+        #Directorio existe pero archivo no
+        touch ~/Documentos/$1/$2
+        echo 'Archivo creado'
+        nano ~/Documentos/$1/$2 
+    fi
+ else
+    #Ni directorio ni archivo existen
+    mkdir ~/Documentos/$1
+    echo 'Directorio creado'
+    touch ~/Documentos/$1/$2
+    echo 'Archivo creado'
+    nano ~/Documentos/$1/$2     
+ fi 
 fi
 fi
